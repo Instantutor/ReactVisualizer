b9a63da523f03a656e4aec3ff4b101a7e6b7bb52
jdawesomeguy <40191080+jdawesomeguy@users.noreply.github.com>
2022-08-15 16:52:04 -0400

    Moving data to JSON
    Working on moving the data from the JSON file into arrays that are used for the graph. Still needs some work, but I'm posting this here so I can save my work.

--- frontend/visualizer/src/Component/intake.js
+++ frontend/visualizer/src/Component/intake.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Graph from "react-graph-vis";
+import data from './data.json';
 
 const options = {
   autoResize: true,
@@ -26,36 +27,25 @@ const options = {
 }
 
 const Intake = () => {
+
+  const n_array = [];
+  const e_array = [];
+
+  for (var k in data.nodes) {
+
+    n_array.push(k);
+  }
+
+  for (var j in data.edges) {
+
+    e_array.push(j);
+  }
+
     const [state] = useState({
         counter: 5,
         graph: {
-            nodes: [
-                {id: 1, label: "New request made by users"},
-                {id: 2, label: "Student selects tutor to ping"},
-                {id: 3, label: "Tutor sees request"},
-                {id: 4, label: "Student can give final confirmation"},
-                {id: 5, label: "Tutor is removed from the student's list"},
-                {id: 6, label: "Tutor is removed from the student's list"},
-                {id: 7, label: "Tutoring in progress"},
-                {id: 8, label: "Student cancelled"},
-                {id: 9, label: "Tutor cancelled"},
-                {id: 10, label: "Request closed"},
-                {id: 11, label: "Student rated"},
-                {id: 12, label: "Tutor rated"},
-            ],
-            edges: [
-                {from: 1, to: 2},
-                {from: 2, to: 3},
-                {from: 3, to: 4, label: "Tutor Accepts Request"},
-                {from: 3, to: 5, label: "Tutor Denies Request"},
-                {from: 4, to: 6, label: "Student chooses a different tutor or denies"},
-                {from: 4, to: 7, label: "Student confirms tutor"},
-                {from: 7, to: 8, label: "Student cancels"},
-                {from: 7, to: 9, label: "Tutor cancels"},
-                {from: 7, to: 10, label: "Tutor or student closes request"},
-                {from: 10, to: 11, label: "Student rates"},
-                {from: 10, to: 12, label: "Tutor rates"}
-            ]
+            nodes: n_array,
+            edges: e_array
         },
         events: {
             select: ({ nodes, edges}) => {
